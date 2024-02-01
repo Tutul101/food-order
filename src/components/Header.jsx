37f8d52224b75479fa07bfb155daf401352a9e8e
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-02-02 00:44:21 +0530

    fetch meals data and show all the meals

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import logo from "../assets/logo.jpg";
+import Button from "./UI/Button";
 const Header = () => {
   return (
     <header id="main-header">
@@ -9,7 +10,7 @@ const Header = () => {
         <h1>React Food</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
       </nav>
     </header>
   );
